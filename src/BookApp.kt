83b7914cb890b6119f96b2d668f397a8e189088c
emilbagirov30 <emil_bgr@mail.ru>
2024-09-17 14:15:48 +0300

    added the creation of a list of books and the start function

---
+++ src/BookApp.kt
@@ -0,0 +1,10 @@
+import javafx.application.Application
+import javafx.stage.Stage
+
+class BookApp : Application() {
+    private var books = createBookList()
+    override fun start(primaryStage: Stage) {
+
+    }
+
+}
