77ee472d2e6d78d709feb1e16ad02cf6d2282685
emilbagirov30 <emil_bgr@mail.ru>
2024-09-17 14:21:11 +0300

    add buttons

--- src/BookApp.kt
+++ src/BookApp.kt
@@ -1,5 +1,6 @@
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
@@ -8,6 +9,11 @@ class BookApp : Application() {
     override fun start(primaryStage: Stage) {
         val root = VBox()
         val scene = Scene(root, 600.0, 400.0)
+
+        val addBook = Button("Добавить книгу")
+        val showBooks = Button("Показать все книги")
+        val filterBooks = Button("Фильтрация книг по году")
+        val sortBooks = Button("Сортировка по названию")
     }
 
 }
