450068fa14eee61c766f570d7803f6cd6e560c3d
emilbagirov30 <emil_bgr@mail.ru>
2024-09-17 12:55:38 +0300

    add fun createBook with auto-increase id

--- src/Book.kt
+++ src/Book.kt
@@ -1,5 +1,9 @@
-var idCounter = 0
+private var idCounter = 0
 data class Book (val id:Int,
                  val title:String,
                  val author:String,
                  val year:Int)
+
+private fun createBook(title: String, author: String, year: Int): Book {
+    return Book(++idCounter, title, author, year)
+}
