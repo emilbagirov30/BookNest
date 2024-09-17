3358045234f79cbc68e2f136b517ee15a8712b43
emilbagirov30 <emil_bgr@mail.ru>
2024-09-17 14:18:07 +0300

    add Scene

--- src/BookApp.kt
+++ src/BookApp.kt
@@ -1,10 +1,13 @@
 import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class BookApp : Application() {
     private var books = createBookList()
     override fun start(primaryStage: Stage) {
-
+        val root = VBox()
+        val scene = Scene(root, 600.0, 400.0)
     }
 
 }
